7c55b843a9af0b0141acd3c4e634f3c2089e17d7
pheelip008 <pheelipraipure@gmail.com>
2026-04-09 02:00:00 +0530

    let's hangout!

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li>home</li>
+                <li>about</li>
+                <li>contact</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
