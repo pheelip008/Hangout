6b2061c33b2291a27ce33ce862212ea0454caeb2
pheelip008 <pheelipraipure@gmail.com>
2026-04-11 14:37:18 +0530

    structuring this shi

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../pagescss/Login.css'
+
+function Login(){
+    return(
+        <>
+        <div>
+            Login page;
+        </div>
+        </>
+    )
+}
+export default Login;
---
+++ client/src/pages/Meeting.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../pagescss/Meeting.css'
+
+function Meeting(){
+    return(
+        <>
+        <div>
+            Meeting page;
+        </div>
+        </>
+    )
+}
+export default Meeting;
